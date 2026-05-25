c0d76d8217cfb9e8b45f81e81e39c521c2716511
luuphongdu <duluu25092005@gmail.com>
2026-05-25 08:34:28 +0700

    add lab02

--- lab-01/ex02_02.py
+++
@@ -1,8 +0,0 @@
-# Nhập bán kính từ người dùng và chuyển sang kiểu số thực (float)
-ban_kinh = float(input("Nhập bán kính của hình tròn: "))
-
-# Tính diện tích theo công thức: S = Pi * r^2
-dien_tich = 3.14 * (ban_kinh ** 2)
-
-# In diện tích của hình tròn ra màn hình
-print("Diện tích của hình tròn là:", dien_tich)
